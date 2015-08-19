1a095a8a6a3c9befd0a522f26fb94c2efafcb5b2
lonelytango <doomon@gmail.com>
2015-08-19 05:01:38 -0400

    .gitignore is now working

--- Pods/Target Support Files/Pods-FourSquareAPITests/Pods-FourSquareAPITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FourSquareAPITests : NSObject
-@end
-@implementation PodsDummy_Pods_FourSquareAPITests
-@end
--- Pods/Target Support Files/Pods-FourSquareAPITests/Pods-FourSquareAPITests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_FourSquareAPITestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_FourSquareAPITestsVersionString[];
-
